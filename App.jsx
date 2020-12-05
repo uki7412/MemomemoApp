19dd4a28092746c842e71c2c9892e0479b9f7f50
Yuki Matsumoto <la.ra.ra.7412@gmail.com>
2020-12-05 12:11:24 +0800

    after rm node.module package-lock.json once and cache clean due to a simulator error

---
+++ App.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-named-as-default-member */
+// import React from 'react';
+// import { StyleSheet, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import MemoListScreen from './src/screens/MemoListScreen';
+
+// import Appbar from './src/components/Appbar';
+// import SignupScreen from './src/screens/SignupScreen';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Appbar />
+//       <SignupScreen />
+//     </View>
+//   );
+// }
+
+const App = createStackNavigator({
+  Home: { screen: MemoListScreen },
+});
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#454545',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     paddingTop: 78,
+//   },
+// });
+
+export default App;
